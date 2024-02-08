38c467485f12dd73835561bf5566f2f304932ff8
Yang, Yuchen <yangyuc@iu.edu>
2024-02-07 20:30:07 -0500

    A3_ducks-service

--- src/test/java/yangyuc/ducksservice/repository/DucksRepositoryTest.java
+++ src/test/java/yangyuc/ducksservice/repository/DucksRepositoryTest.java
@@ -58,9 +58,13 @@ class DucksRepositoryTest {
     void searchDuck() throws FileNotFoundException {
         Duck.DuckData duck = new Duck.DuckData(1, Type.MALLARD.toString());
         Duck.DuckData duck2 = new Duck.DuckData(2, Type.REDHEAD.toString());
+        Duck.DuckData duck3 = new Duck.DuckData(3, Type.RUBBER_DUCK.toString());
+        Duck.DuckData duck4 = new Duck.DuckData(4, Type.DECOY_DUCK.toString());
         ArrayList<Duck.DuckData> ducks = new ArrayList<>();
         ducks.add(duck);
         ducks.add(duck2);
+        ducks.add(duck3);
+        ducks.add(duck4);
         assertEquals(ducks, DucksRepository.searchDuck(new Duck.DuckData(null, null)));
         assertEquals(duck, DucksRepository.searchDuck(new Duck.DuckData(1, null)).get(0));
         assertEquals(duck2, DucksRepository.searchDuck(new Duck.DuckData(null, Type.REDHEAD.toString())).get(0));
